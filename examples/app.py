684253225d48f28a0efbbae71b99dea44601c79e
pmasa <masamuna.pedro@hotmail.com>
2020-04-05 21:39:40 +0200

    module gauge introduced

--- examples/app.py
+++ examples/app.py
@@ -1,28 +1,12 @@
 #!/usr/bin/python
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
+from flask import Flask, render_template, request
+import requests
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+import redis
 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 import sys
 
 import Adafruit_DHT
-
 
 # Parse command line parameters.
 sensor_args = { '11': Adafruit_DHT.DHT11,
@@ -36,28 +20,33 @@ else:
     print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
     sys.exit(1)
 
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+app = Flask(__name__)
 
-# Un-comment the line below to convert the temperature to Fahrenheit.
-# temperature = temperature * 9/5.0 + 32
+try:
+    r = redis.Redis(host="localhost",port=6379)
+    r.ping()
+except redis.ConnectionError:
+    exit('Failed to connect to Redis, terminating.')                       
 
-# Note that sometimes you won't get a reading and
-# the results will be null (because Linux can't
-# guarantee the timing of calls to read the sensor).
-# If this happens try again!
+@app.route('/')
+def index():
+    print("index !!!!")
+    return render_template('index.html')
 
-#if humidity is not None and temperature is not None:
-#    print('Temp+={0:0.1f}*  Humidity+={1:0.1f}%'.format(temperature, humidity))
-#else:
-#    print('Failed to get reading. Try again!')
-#    sys.exit(1)
-i = 1
-while i < 6:
-  if humidity is not None and temperature is not None:
-      print('Temp+={0:0.1f}*  Humidity+={1:0.1f}%'.format(temperature, humidity))
-  else:
-      print('Failed to get reading. Try again!')
-  print(i)
-  i += 1
+
+@app.route('/suggestions')
+def suggestions():
+    print("suggestions !!!!")
+    text = request.args.get('jsdata')
+    print(text)
+
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+    if humidity is None or temperature is None:
+        print("humidity or temperature is None")
+        text = r.get("humidity")
+    r.set("humidity",humidity)
+    return render_template('suggestions.html', suggestions=humidity)
+
+if __name__ == '__main__':
+    app.run(debug=True)
